0b41fb1da8d45fb4cec9716247e41dd25c5b02fb
Kris <kris.bierma@gmail.com>
2020-04-13 17:22:30 -0500

    styling

---
+++ public/scripts/gamePlay.js
@@ -0,0 +1,20 @@
+// while health !=0 and socail != 0 and level < =5
+
+let level = 1
+let storyboard = level1
+let storyArr = [level1, level2, level3];
+let index = 0;
+
+// play game
+
+// show image
+// grab the html spot and put the image
+// storyArr[index].images.main
+// level1.images.main
+
+// show desc
+// show choice
+// user choose one
+// show desc
+// update health
+// update social
