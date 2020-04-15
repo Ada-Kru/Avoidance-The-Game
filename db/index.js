01725dd1f61ac67067bbc2c7cbc7f79cfb2dec24
Adam Krueger <adamkru@gmail.com>
2020-04-15 15:45:35 -0500

    Removed hardcoded database connection.  Now uses environment variables to connect.

--- db/index.js
+++ db/index.js
@@ -1,12 +1,6 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    host: "localhost",
-    user: "postgres",
-    password: "postgres",
-    port: 5432,
-    database: "Avoidance",
-});
+const pool = new Pool();
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
