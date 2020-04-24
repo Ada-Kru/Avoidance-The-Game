c0840e3b54f3be8569fcf8ed5e61cd80c4cd09b4
Adam Krueger <adamkru@gmail.com>
2020-04-24 15:34:25 -0500

    Added images, play page changes images, refactored some gameplay.js code.

--- public/scripts/gamePlay.js
+++ public/scripts/gamePlay.js
@@ -1,85 +1,81 @@
-// grab initial health and social points from landing page 
+// grab initial health and social points from landing page
 let health = 50;
 let social = 50;
-let level = 1;
-let i = level - 1;
+let level = 0;
 let sublevel = 0;
 
 // update aria-valuenow with health and social
-$('#health_points').text(`${health} / 100`);
-$('#social_points').text(`${social} / 100`);
-$('#progress-bar__health').attr('aria-valuenow', health).css({'width': `${health}%`});
-$('#progress-bar__social').attr('aria-valuenow', social).css({'width': `${social}%`});
+$("#health_points").text(`${health} / 100`);
+$("#social_points").text(`${social} / 100`);
+$("#progress-bar__health")
+  .attr("aria-valuenow", health)
+  .css({ width: `${health}%` });
+$("#progress-bar__social")
+  .attr("aria-valuenow", social)
+  .css({ width: `${social}%` });
 
-playGame(level, health, social, i, sublevel);
+// add continue button
+let continueBtn = $("<button>Continue</button>");
+continueBtn.attr({ id: "continue", type: "button" });
+continueBtn.addClass("btn btn__choice btn-lg btn-block");
+continueBtn.click(() => {
+  playGame(level, health, social, sublevel);
+});
+$(".grid__choices").append(continueBtn);
 
-  // user chooses one
-for (let choiceIndex of [0, 1, 2]) {
-  $(`#choice${choiceIndex + 1}`).click(() => { 
-    let healthModifier = levels.points[i][choiceIndex][0];
-    let socialModifier = levels.points[i][choiceIndex][1];
+// user chooses one
+for (let choiceIndex = 0; choiceIndex < 3; choiceIndex++) {
+  $(`#choice${choiceIndex + 1}`).click(() => {
+    let healthModifier = levels.points[level][choiceIndex][0];
+    let socialModifier = levels.points[level][choiceIndex][1];
 
     // update health and social points
     health += healthModifier;
     social += socialModifier;
-    $('#health_points').text(`${health} / 100`);
-    $('#progress-bar__health').attr('aria-valuenow', health).css({'width': `${health}%`});
-    $('#social_points').text(`${social} / 100`);
-    $('#progress-bar__social').attr('aria-valuenow', social).css({'width': `${social}%`});
+    $("#health_points").text(`${health} / 100`);
+    $("#progress-bar__health")
+      .attr("aria-valuenow", health)
+      .css({ width: `${health}%` });
+    $("#social_points").text(`${social} / 100`);
+    $("#progress-bar__social")
+      .attr("aria-valuenow", social)
+      .css({ width: `${social}%` });
 
     // show desc of choice
-    $('#description').text(levels.choiceAnswers[i][choiceIndex]);
-    
-    // hide choice buttons
-    $('.btn__choice').hide();
+    $("#description").text(levels.choiceAnswers[i][choiceIndex]);
 
     // update level/sublevel
-    if (sublevel < 3) {
+    if (sublevel < 1) {
       sublevel++;
-    }
-    else {
+    } else {
       sublevel = 0;
       level++;
     }
 
-    // add next level button
-    let nextLevel = $('<button>');
-    nextLevel.attr('id', 'nextLevel');
-    nextLevel.attr('type', 'button');
-    nextLevel.addClass('btn btn__next btn-lg btn-block');
-    nextLevel.text('Next Level');
-    nextLevel.click(() => {
-      playGame(level, health, social, i, sublevel)
-      $('.btn__next').hide();
-      $('.btn__choice').show();
-    });
-    $('.grid__choices').append(nextLevel);
+    // hide choice buttons, show continue button
+    $(".btn__choice").hide();
+    $("#continue").show();
   });
 }
 
-  //use ajax for pics?
+playGame(level, health, social, sublevel);
 
-  let pic = $('<img>');
-  pic.attr('src', '../public/images/street.jpg');
-  // pic.attr('alt', 'Street between buildings with people walking on it');
+function playGame(level, health, social, sublevel) {
+  $(".btn__choice").show();
+  $("#continue").hide();
 
-  // $('.grid__health').prepend(pic);
-  // $('.grid__image').append(pic);
-
-function playGame(level, health, social, i, sublevel) {
-
-  console.log(i, sublevel);
   // play game
-  if (health != 0 && social != 0 && level <=5) {
+  if (health != 0 && social != 0 && level <= 4) {
     // show image
-    $('#image_main').append(levels.images[i][sublevel]);
+    console.log(level, sublevel);
+    $("#main_img").attr("src", `images/${levels.images[level][sublevel][0]}`);
 
     // show desc
-    $('#description').text(levels.descriptions[i][sublevel]);
+    $("#description").text(levels.descriptions[level][sublevel]);
 
     // show choices
-    $('#choice1').text(levels.choices[i][sublevel]);
-    $('#choice2').text(levels.choices[i][sublevel + 1]);
-    $('#choice3').text(levels.choices[i][sublevel + 2]);
+    $("#choice1").text(levels.choices[level][sublevel]);
+    $("#choice2").text(levels.choices[level][sublevel + 1]);
+    $("#choice3").text(levels.choices[level][sublevel + 2]);
   }
 }
--- public/scripts/storyBoard.js
+++ public/scripts/storyBoard.js
@@ -2,39 +2,68 @@ const levels = {
   levelNum: [1, 2, 3, 4, 5],
   images: [
     [
-      "street.jpg", "", ""
+      ["level1-1-background1.jpg", "", ""],
+      ["level1-1-background1.jpg", "", ""],
     ],
-    [], [], [], []
+    [
+      ["level2-1-background1.jpg", "", ""],
+      ["level2-2-background1.jpg", "", ""],
+    ],
+    [
+      ["level3-1-background1.jpg", "", ""],
+      ["level3-2-background1.jpg", "", ""],
+    ],
+    [
+      ["level4-1-background1.jpg", "", ""],
+      ["level4-2-background1.jpg", "", ""],
+    ],
+    [
+      ["level5-1-background1.jpg", "", ""],
+      ["level5-2-background1.jpg", "", ""],
+    ],
   ],
 
   descriptions: [
     [
       "Being in an essential job, you must go to work today.",
       "Who’s that? It’s grandma! Hello! she calls out to you. What do you do?",
-      ""
+      "",
     ],
-    [], [], [], []
-],
+    [],
+    [],
+    [],
+    [],
+  ],
 
   choices: [
     [
       "Turn off on a side street.",
       "Smile, wave and walk faster.",
-      "Give her a quick hug, explain (again) what’s going on and how important social distancing is."
+      "Give her a quick hug, explain (again) what’s going on and how important social distancing is.",
     ],
-    [], [], [], []
-],
+    [],
+    [],
+    [],
+    [],
+  ],
   choiceAnswers: [
     [
       "You quickly turn off onto a side street. Unfortunately, grandma doesn’t understand.",
       "Grandma smiles and waves back. Good job avoiding social interaction but also keeping grandma happy.",
-      "Grandma squeezes tightly. You feel so loved and also a little germy."
-    ]
+      "Grandma squeezes tightly. You feel so loved and also a little germy.",
     ],
+  ],
   points: [
-            [ [-30, 20], [15, 0], [30, -30] ],
-              [], [], [], []
-          ]
+    [
+      [-30, 20],
+      [15, 0],
+      [30, -30],
+    ],
+    [],
+    [],
+    [],
+    [],
+  ],
 };
 
 const level1 = {
@@ -42,40 +71,41 @@ const level1 = {
   images: {
     main: "",
     clipart1: [],
-    clipart2: []
+    clipart2: [],
   },
   descriptions: {
     main: "Being in an essential job, you must go to work today.",
-    sub1: "Who’s that? It’s grandma! Hello! she calls out to you. What do you do?",
-    sub2: ""
+    sub1:
+      "Who’s that? It’s grandma! Hello! she calls out to you. What do you do?",
+    sub2: "",
   },
   choices: {
     sub1: {
       1: "Turn off on a side street.",
       2: "Smile, wave and walk faster.",
-      3: "Give her a quick hug, explain (again) what’s going on and how important social distancing is."
+      3: "Give her a quick hug, explain (again) what’s going on and how important social distancing is.",
     },
     sub2: {
       1: "",
       2: "",
-      3: ""  
-    }
+      3: "",
+    },
   },
   choiceAnswers: {
     1: "You quickly turn off onto a side street. Unfortunately, grandma doesn’t understand.",
     2: "Grandma smiles and waves back. Good job avoiding social interaction but also keeping grandma happy.",
-    3: "Grandma squeezes tightly. You feel so loved and also a little germy."  
+    3: "Grandma squeezes tightly. You feel so loved and also a little germy.",
   },
   points: {
     sub1: {
       1: [-30, 20],
       2: [15, 0],
-      3: [30, -30]
+      3: [30, -30],
     },
     sub2: {
       1: [],
       2: [],
-      3: []      
-    }
-  }
+      3: [],
+    },
+  },
 };
